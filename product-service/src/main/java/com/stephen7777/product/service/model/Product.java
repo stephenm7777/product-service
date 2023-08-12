5036921e56c87746addc20e34e8d9caeaaf14fb9
stephenm7777 <stephenmartinez7000@gmail.com>
2023-08-11 18:54:47 -0700

    Update to Product.java

--- product-service/src/main/java/com/stephen7777/product/service/model/Product.java
+++ product-service/src/main/java/com/stephen7777/product/service/model/Product.java
@@ -1,5 +1,25 @@
 package com.stephen7777.product.service.model;
 
-public class Product {
+import org.springframework.data.annotation.Id; 
+import org.springframework.data.mongodb.core.mapping.Document; 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Builder; 
+import lombok.Data; 
 
+import java.math.BigDecimal;
+
+@Document(value = "product")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder 
+@Data 
+
+public class Product {
+	@Id
+	private String id; 
+	private String name; 
+	private String description; 
+	private BigDecimal price; 
+	
 }
